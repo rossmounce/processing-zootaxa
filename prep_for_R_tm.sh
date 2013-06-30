2746be4f836ca0ec0118b12098e394e5eb0fa0d6
Ross Mounce <ross.mounce@gmail.com>
2013-06-30 22:22:35 +0100

    Create prep_for_R_tm.sh

---
+++ prep_for_R_tm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Usage: batch prep .txt files for use with tm package in R
+# Outputs: one string per line files as .ooo of all strings >3, with custom word-stemming
+
+shopt -s nullglob
+
+# Make list of all files
+ls *.txt > lstxt.ttt
+
+# Loop through pwd cd'ing into each directory then pdftotext all PDFs within each subdirectory
+for i in $(cat lstxt.ttt); do
+  echo "word bag for $i"
+        tac "$i" |                    #reverse cat each file, then grep for Refs and print only after this
+        egrep "(Literature [cC]ited$|References$|REFERENCES$|Bibliography$|BIBLIOGRAPHY$|LITERATURE CITED$)" -m 1 -A 99999 | 
+        tac | strings |               #removes special characters e.g Copyright symbol and other strangeness
+        tr '[A-Z]' '[a-z]' |          #change to all lowercase
+        sed 's/[[:punct:]]/ /g' |     #remove all punctuation
+        sed s/' '/\\n/g |             #replace spaces with newlines (each string on separate line)
+        sort | awk 'length > 3' |     #sort strings & remove string counts of 3 or less characters e.g. 'the'
+        sed -r 's/phylog.+/phylogSTEM/g' |    #custom stemming  phylogenetic, phylogeny, phylogentically, phylogenetics
+        sed -r 's/cladog.+/cladistii/g' |     #stemming cladogram, cladograms, cladogenetic
+        sed -r 's/cladist.+/cladistSTEM/g' |  #stemming cladistic, cladistically, cladistics
+        sed -r 's/parsimon.+/parsimonSTEM/g'  > "$i.ooo" 
+	done
